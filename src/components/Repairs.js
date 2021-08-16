140b9e483dcd58db79c0988bc29ab78e16d91d5b
ts <shanksleslietrey@gmail.com>
2021-08-16 14:12:00 -0500

    added navbar with react router

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,21 +1,13 @@
 import React from "react"
-import { CustomerList } from "./customers/CustomerList";
-import { EmployeeList } from "./employees/EmployeeList";
-import { TicketList } from "./serviceTickets/TicketList";
+import { NavBar } from "./nav/NavBar";
+import { ApplicationViews } from "./ApplicationViews";
 
 export const Repairs = () => {
     return (
         <>
+            <NavBar />
             <h1>Honey Rae's Repair Shop</h1>
-
-            <h2>Customer List</h2>
-            <CustomerList />
-            
-            <h2>Employee List</h2>
-            <EmployeeList />
-    
-            <h2>Ticket List</h2>
-            <TicketList />
+            <ApplicationViews />
         </>
     )
 }
